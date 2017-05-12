2e256573202945f38d8930073634e246fc59c9a5
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-12 04:02:48 +0200

    initial commit

---
+++ src/module.ts
@@ -0,0 +1,54 @@
+import { IAllocateRequest, IAllocateResponse, IDeallocateNotification, IWorkerEvent } from 'async-array-buffer-worker';
+
+const MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || Math.pow(2, 53) - 1;
+
+const generateUniqueId = (set: Set<number>) => {
+    let id = Math.round(Math.random() * MAX_SAFE_INTEGER);
+
+    while (set.has(id)) {
+        id = Math.round(Math.random() * MAX_SAFE_INTEGER);
+    }
+
+    return id;
+};
+
+export const load = (url: string) => {
+    const worker = new Worker(url);
+
+    const ongoingRecordingRequests: Set<number> = new Set();
+
+    const allocate = (length: number): Promise<ArrayBuffer> => {
+        return new Promise((resolve, reject) => {
+            const id = generateUniqueId(ongoingRecordingRequests);
+
+            ongoingRecordingRequests.add(id);
+
+            const onMessage = ({ data }: IWorkerEvent) => {
+                if (data.id === id) {
+                    ongoingRecordingRequests.delete(id);
+
+                    worker.removeEventListener('message', onMessage);
+
+                    if (data.error === null) {
+                        resolve((<IAllocateResponse> data).result.arrayBuffer);
+                    } else {
+                        reject(new Error(data.error.message));
+                    }
+                }
+            };
+
+            worker.addEventListener('message', onMessage);
+
+            worker.postMessage(<IAllocateRequest> { id, method: 'allocate', params: { length } });
+        });
+    };
+
+    const deallocate = (arrayBuffer: ArrayBuffer) => {
+        worker.postMessage(<IDeallocateNotification> { id: null, method: 'deallocate', params: { arrayBuffer } });
+    };
+
+    return {
+        allocate,
+        deallocate
+    };
+};
---
+++ test/unit/module.js
@@ -0,0 +1,106 @@
+import { load } from '../../src/module';
+
+describe('module', () => {
+
+    let asyncArrayBuffer;
+
+    afterEach(() => {
+        Worker.reset();
+    });
+
+    beforeEach(() => {
+        Worker = ((OriginalWorker) => { // eslint-disable-line no-global-assign
+            const instances = [];
+
+            return class ExtendedWorker extends OriginalWorker {
+
+                constructor (url) {
+                    super(url);
+
+                    const addEventListener = this.addEventListener;
+
+                    // This is an ugly hack to prevent the broker from handling mirrored events.
+                    this.addEventListener = (index, ...args) => {
+                        if (typeof index === 'number') {
+                            return addEventListener.apply(this, args);
+                        }
+                    };
+
+                    instances.push(this);
+                }
+
+                static addEventListener (index, ...args) {
+                    return instances[index].addEventListener(index, ...args);
+                }
+
+                static get instances () {
+                    return instances;
+                }
+
+                static reset () {
+                    Worker = OriginalWorker; // eslint-disable-line no-global-assign
+                }
+
+            };
+        })(Worker);
+
+        const blob = new Blob([`
+            self.addEventListener('message', ({ data }) => {
+                self.postMessage(data);
+            });
+        `], { type: 'application/javascript' });
+
+        asyncArrayBuffer = load(URL.createObjectURL(blob));
+    });
+
+    describe('allocate()', () => {
+
+        let length;
+
+        beforeEach(() => {
+            length = 1024;
+        });
+
+        it('should send the correct message', (done) => {
+            Worker.addEventListener(0, 'message', ({ data }) => {
+                expect(data.id).to.be.a('number');
+
+                expect(data).to.deep.equal({
+                    id: data.id,
+                    method: 'allocate',
+                    params: { length }
+                });
+
+                done();
+            });
+
+            asyncArrayBuffer.allocate(length);
+        });
+
+    });
+
+    describe('deallocate()', () => {
+
+        let arrayBuffer;
+
+        beforeEach(() => {
+            arrayBuffer = new ArrayBuffer(2048);
+        });
+
+        it('should send the correct message', (done) => {
+            Worker.addEventListener(0, 'message', ({ data }) => {
+                expect(data).to.deep.equal({
+                    id: null,
+                    method: 'deallocate',
+                    params: { arrayBuffer }
+                });
+
+                done();
+            });
+
+            asyncArrayBuffer.deallocate(arrayBuffer);
+        });
+
+    });
+
+});
